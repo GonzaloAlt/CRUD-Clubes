15b3feea304ac196b890b4f6c8c0f90168a6802b
Gonzalo Altamirano <gonza12683@gmail.com>
2022-08-24 17:04:46 -0300

    Set club entity
    Need to split area in another module.

---
+++ src/module/club/entity/club.js
@@ -0,0 +1,18 @@
+class Club{
+    constructor(
+        id, area, name, shortName, tla, crest, address, website, founded, clubColors, venue, lastUpdated
+    ){
+        this.id = id;
+        this.area = area;
+        this.name = name;
+        this.shortName = shortName;
+        this.tla = tla;
+        this.crest = crest;
+        this.address = address;
+        this.website = website;
+        this.founded= founded;
+        this.clubColors = clubColors;
+        this.venue = venue;
+        this.lastUpdated = lastUpdated;
+    }
+}
