5ec9c295aa21219d3e91e041c6f7b9d437398055
Gonzalo Altamirano <gonza12683@gmail.com>
2022-08-24 17:01:32 -0300

    Create club service

---
+++ src/module/club/service/clubService.js
@@ -0,0 +1,57 @@
+class Service{
+    /**
+     * 
+     * @param {import('../repository/clubRepository')} clubRepository 
+     */
+    constructor(clubRepository){
+        this.clubRepository = clubRepository;
+    }
+
+    async save(club){
+        try {
+            return this.clubRepository.save(club)
+        } catch (error) {
+            throw Error(error)
+        }
+    }
+    async getById(id){
+        try {
+            return this.clubRepository.getById(id)
+        } catch (error) {
+            throw Error(error)
+        }
+    }
+    async getAll(){
+        try {
+            return this.clubRepository.getAll()
+        } catch (error) {
+            throw Error(error)
+        }
+    }
+
+    async deleteById(id){
+        try {
+            return this.clubRepository.deleteById(id)
+        } catch (error) {
+            throw Error(error)
+        }
+    }
+
+    async deleteAll(){
+        try {
+            return this.clubRepository.deleteAll()
+        } catch (error) {
+            throw Error(error)
+        }
+    }
+
+    async update(id, info){
+        try {
+            return this.clubRepository.update(id, info)
+        } catch (error) {
+            throw Error(error)
+        }
+    }
+}
+
+module.exports = Service;
