97116336521bf038c15e2b4fe7dc1d21b5bb36d0
Gonzalo Altamirano <gonza12683@gmail.com>
2022-08-24 17:00:10 -0300

    Set Dependency Inyection Container

---
+++ src/config/di.js
@@ -0,0 +1,72 @@
+const { object, use, factory, func, default: DIContainer } = require('rsdi');
+const {ClubController, ClubService, ClubRepository, ClubRoutes} = require('../module/club/clubModule');
+const fs = require('fs');
+const { v4: uuid } = require('uuid');
+const multer = require('multer');
+const session = require('express-session')
+
+function configSession(){
+    return session ({
+        secret: process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: true,
+        cookie: { maxAge: 60000 }
+      })
+}
+
+function configMulter(){
+    const storage = multer.diskStorage({
+        destination: function (req, file, cb) {
+          cb(null, 'uploads')
+        },
+        filename: function (req, file, cb) {
+          cb(null, file.fieldname + '-' + Date.now())
+        }
+      })
+       
+      const upload = multer({ storage: storage })
+      return upload;
+}
+
+function configUuid(){
+    return uuid
+}
+
+function configDB(){
+    return process.env.DB_PATH;
+}
+
+/** 
+ * @param {DIContainer} container 
+ */
+
+function addCommonDefinitions(container){
+    container.add({
+        fs,
+        uuid: factory(configUuid),
+        multer: factory(configMulter),
+        session: factory(configSession),
+        db: factory(configDB),
+    })
+}
+
+/** 
+ * @param {DIContainer} container 
+ */
+function addClubModuleDefinitions(container){
+    container.add({
+        ClubRoutes: object(ClubRoutes).construct(use('ClubController')),
+        ClubController: object(ClubController).construct(use('ClubService'), use('session'), use('multer')),
+        ClubService: object(ClubService).construct(use('ClubRepository')),
+        ClubRepository: object(ClubRepository).construct(use('fs'), use('uuid'), use('db')),
+    })
+}
+
+const configureDI = ()=>{
+    const container = new DIContainer();
+    addCommonDefinitions(container);
+    addClubModuleDefinitions(container);
+    return container
+}
+
+module.exports = configureDI;
