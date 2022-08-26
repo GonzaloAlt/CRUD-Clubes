991235075974472ba9da9cbbe538f06a5fbd84a3
Gonzalo Altamirano <gonza12683@gmail.com>
2022-08-26 14:55:08 -0300

    Update areaService.js

--- src/module/area/service/areaService.js
+++ src/module/area/service/areaService.js
@@ -1,32 +1,56 @@
 class Service{
     /**
      * 
-     * @param {import('../repository/areaRepository')} repository 
+     * @param {import('../repository/areaRepository')} areaRepository 
      */
-    constructor(repository){
-        this.repository = repository;
+    constructor(areaRepository){
+        this.areaRepository = areaRepository;
     }
 
     async save(area){
-
+        try {
+            return this.areaRepository.save(area)
+        } catch (error) {
+            throw Error(error)
+        }
     }
     async getById(id){
-
+        try {
+            return this.areaRepository.getById(id)
+        } catch (error) {
+            throw Error(error)
+        }
     }
     async getAll(){
-
+        try {
+            return this.areaRepository.getAll()
+        } catch (error) {
+            throw Error(error)
+        }
     }
 
     async deleteById(id){
-
+        try {
+            return this.areaRepository.deleteById(id)
+        } catch (error) {
+            throw Error(error)
+        }
     }
 
     async deleteAll(){
-
+        try {
+            return this.areaRepository.deleteAll()
+        } catch (error) {
+            throw Error(error)
+        }
     }
 
     async update(id, info){
-
+        try {
+            return this.areaRepository.update(id, info)
+        } catch (error) {
+            throw Error(error)
+        }
     }
 }
 
