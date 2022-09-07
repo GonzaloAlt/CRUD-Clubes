ae2e664824a22e49cce8f067fa947f2ac6d0afde
Gonzalo Altamirano <gonza12683@gmail.com>
2022-09-07 10:36:47 -0300

    Fix view fn
    Get area club in club service.

--- src/module/club/controller/clubController.js
+++ src/module/club/controller/clubController.js
@@ -4,7 +4,7 @@ class ClubController {
      * @param {import('../service/clubService')} clubService 
      * @param {import('../../area/service/areaService')} areaService 
      */
-    constructor(clubService, areaService){
+    constructor(clubService, areaService) {
         this.clubService = clubService;
         this.areaService = areaService;
     }
@@ -13,15 +13,15 @@ class ClubController {
      * @param {import('express').Request} req
      * @param {import('express').Response} res
      */
-    async create(req, res){
+    async create(req, res) {
         try {
-            const {id} = req.params;
+            const { id } = req.params;
             const areas = await this.areaService.getAll()
-            if(id){
+            if (id) {
                 const club = await this.clubService.getById(id)
-                res.render('club/views/clubForm.html', {club,id, areas})
-            }else{
-                res.render('club/views/clubForm.html', {areas}) ;  
+                res.render('club/views/clubForm.html', { club, id, areas })
+            } else {
+                res.render('club/views/clubForm.html', { areas });
             }
         } catch (error) {
             req.session.errors = [error]
@@ -31,13 +31,13 @@ class ClubController {
      * @param {import('express').Request} req
      * @param {import('express').Response} res
      */
-    async viewAll(req,res){
-        if(req.session.messages=== undefined) req.session.messages = []
-        if(req.session.errors=== undefined) req.session.errors = []
+    async viewAll(req, res) {
+        if (req.session.messages === undefined) req.session.messages = []
+        if (req.session.errors === undefined) req.session.errors = []
 
         try {
             const clubs = await this.clubService.getAll()
-            res.render('club/views/index.html', {clubs, messages:req.session.messages, errors: req.session.errors})
+            res.render('club/views/index.html', { clubs, messages: req.session.messages, errors: req.session.errors })
             req.session.messages = []
             req.session.errors = []
         } catch (error) {
@@ -48,12 +48,11 @@ class ClubController {
      * @param {import('express').Request} req
      * @param {import('express').Response} res
      */
-    async view(req, res){
+    async view(req, res) {
         try {
-            const {id} = req.params;
+            const { id } = req.params;
             const club = await this.clubService.getById(id)
-            const area = await this.areaService.getById(club.area)
-            res.render('club/views/clubOverview.html', {club, area})
+            res.render('club/views/clubOverview.html', { club })
         } catch (error) {
             req.session.errors = [error]
         }
@@ -62,12 +61,15 @@ class ClubController {
      * @param {import('express').Request} req
      * @param {import('express').Response} res
      */
-    async save(req, res){
+    async save(req, res) {
         try {
-            if(req.file){
-                const {path} = req.file;
-                const body = req.body;
+            const body = req.body;
+            if (req.file) {
+                const { path } = req.file;
                 body.crest = `/${path.replace("\\\\", "http://").replace(/\\/g, "/")}`;
+                const clubSaved = await this.clubService.save(body);
+                req.session.messages = [`Se creó el club con id: ${clubSaved.id}`]
+            } else {
                 const clubSaved = await this.clubService.save(body);
                 req.session.messages = [`Se creó el club con id: ${clubSaved.id}`]
             }
@@ -80,17 +82,17 @@ class ClubController {
      * @param {import('express').Request} req
      * @param {import('express').Response} res
      */
-    async update(req, res){
+    async update(req, res) {
         try {
-            const {id} = req.params;
+            const { id } = req.params;
             const body = req.body;
-            if(req.file){
-                const {path} = req.file;
+            if (req.file) {
+                const { path } = req.file;
                 const club = body;
                 club.crest = `/${path.replace("\\\\", "http://").replace(/\\/g, "/")}`;
                 const clubUpdated = await this.clubService.update(id, club);
                 req.session.messages = [`Se actualizó el club con id: ${clubUpdated.id}`]
-            }else{
+            } else {
                 const clubUpdated = await this.clubService.update(id, body);
                 req.session.messages = [`Se actualizó el club con id: ${clubUpdated.id}`]
             }
@@ -103,9 +105,9 @@ class ClubController {
      * @param {import('express').Request} req
      * @param {import('express').Response} res
      */
-    async delete(req, res){
+    async delete(req, res) {
         try {
-            const {id} = req.params;
+            const { id } = req.params;
             const clubDeleted = await this.clubService.deleteById(id);
             req.session.messages = [`Se eliminó el club`]
             res.redirect('/club/')
